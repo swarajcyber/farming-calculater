e8ee5d8b0c877e10b92462693df2e48993ee7223
swarajcyber <87569342+swarajcyber@users.noreply.github.com>
2021-12-04 18:58:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+        function calculate(){
+            number_1=document.getElementById("sides").value
+            var area=number_1*number_1
+            var perimeter=number_1*4
+            document.getElementById(label1).innerHTML=area
+            document.getElementById(label1).innerHTML=perimeter
+        }
+    
+ 
